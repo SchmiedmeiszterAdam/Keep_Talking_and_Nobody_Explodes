076b7ef6267e5714c067164372179998ecdfaa26
SchmiedmeiszterAdam <smikuadam@gmail.com>
2021-11-15 20:58:29 +0100

    simple-wires css, modul színezések

---
+++ time-module/time-modul.js
@@ -0,0 +1,31 @@
+$(function () {
+    let ido = 5
+    let masodperc = 0
+    if (masodperc < 10) {
+        $("#ido").text(ido + ":0" + masodperc)
+    }
+    else {
+        $("#ido").text(ido + ":" + masodperc)
+    }
+    setTimeout(function () {
+        idoKijelzes()
+    }, 2000)
+    function idoKijelzes() {
+        let idoIras = setInterval(function () {
+            masodperc--
+            if (masodperc === -1) {
+                ido--
+                masodperc = 59
+            }
+            if (masodperc < 10) {
+                $("#ido").text(ido + ":0" + masodperc)
+            }
+            else {
+                $("#ido").text(ido + ":" + masodperc)
+            }
+            if (ido === 0 && masodperc === 0) {
+                clearInterval(idoIras)
+            }
+        }, 1000)
+    }
+})
