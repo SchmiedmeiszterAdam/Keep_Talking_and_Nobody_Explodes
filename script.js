b9f29303deafeae42ff2f2778e3bb11112a2d256
SchmiedmeiszterAdam <smikuadam@gmail.com>
2021-11-23 00:40:19 +0100

    2 másodperc késleltetés

--- script.js
+++ script.js
@@ -4,13 +4,15 @@
 // { "template": "#templates .memory", "className": Memory }
 // ]
 $(function () {
-    let bombTemplate = $("#bomba")
-    let bombA = $(bombTemplate).appendTo("main")
-    let bomb = new Bomba(bombA)
-    bomb.createModules($("#templates .simple-wires"),Wire)
-    bomb.createModules($("#templates .whos-on-first"),WhosOnFirst)
-    bomb.createModules($("#templates .keypads"),Keypad)
-    bomb.createModules($("#templates .keypads"),Keypad)
-    bomb.createModules($("#templates .memory"),Memory)
-    bomb.createModules($("#templates .memory"),Memory)
+    setTimeout(()=>{
+        let bombTemplate = $("#bomba")
+        let bombA = $(bombTemplate).appendTo("main")
+        let bomb = new Bomba(bombA)
+        bomb.createModules($("#templates .simple-wires"),Wire)
+        bomb.createModules($("#templates .whos-on-first"),WhosOnFirst)
+        bomb.createModules($("#templates .keypads"),Keypad)
+        bomb.createModules($("#templates .keypads"),Keypad)
+        bomb.createModules($("#templates .memory"),Memory)
+        bomb.createModules($("#templates .memory"),Memory)
+    },2000)
 })
