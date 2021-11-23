92802151edca3798bedc03e9a7eb876702ee9514
SchmiedmeiszterAdam <smikuadam@gmail.com>
2021-11-23 13:35:44 +0100

    mappa nevek javítása

--- script.js
+++ script.js
@@ -4,15 +4,13 @@
 // { "template": "#templates .memory", "className": Memory }
 // ]
 $(function () {
-    setTimeout(()=>{
-        let bombTemplate = $("#bomba")
-        let bombA = $(bombTemplate).appendTo("main")
-        let bomb = new Bomba(bombA)
-        bomb.createModules($("#templates .simple-wires"),Wire)
-        bomb.createModules($("#templates .whos-on-first"),WhosOnFirst)
-        bomb.createModules($("#templates .keypads"),Keypad)
-        bomb.createModules($("#templates .keypads"),Keypad)
-        bomb.createModules($("#templates .memory"),Memory)
-        bomb.createModules($("#templates .memory"),Memory)
-    },2000)
+    let bombTemplate = $("#bomba")
+    let bombA = $(bombTemplate).appendTo("main")
+    let bomb = new Bomba(bombA)
+    bomb.createModules($("#templates .simple-wires"), Wire)
+    bomb.createModules($("#templates .whos-on-first"), WhosOnFirst)
+    bomb.createModules($("#templates .keypads"), Keypad)
+    bomb.createModules($("#templates .keypads"), Keypad)
+    bomb.createModules($("#templates .memory"), Memory)
+    bomb.createModules($("#templates .memory"), Memory)
 })
