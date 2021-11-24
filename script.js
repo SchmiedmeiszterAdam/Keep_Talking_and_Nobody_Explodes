d22684570fb83924d0d66e008775880c2f5abc09
SchmiedmeiszterAdam <smikuadam@gmail.com>
2021-11-24 18:57:20 +0100

    üres modulok hozzáadása a bombához

--- script.js
+++ script.js
@@ -8,22 +8,30 @@ $(function () {
     let bombA = $(bombTemplate).appendTo("main")
     let bomb = new Bomba(bombA)
     
-    let bomb1 = [modulePossibilitys[0],modulePossibilitys[1],modulePossibilitys[2],modulePossibilitys[3]]
-    if(bomb1.length < 6){
-        for (let i = 0; i < 5-bomb1.length; i++) {
-            $("#templates .ures").clone().appendTo(bomb.gyerek)
-        }
-    }
-    else{
-        for (let i = 0; i < 11-bomb1.length; i++) {
-            $("#templates .ures").clone().appendTo(bomb.gyerek)
-        }
-    }
+    let bomb1 = [modulePossibilitys[0],modulePossibilitys[0],modulePossibilitys[0],modulePossibilitys[1],modulePossibilitys[2],modulePossibilitys[3],modulePossibilitys[3]]
     keveres(bomb1)
     for (let i = 0; i < bomb1.length; i++) {
         bomb.createModule($(bomb1[i].template), bomb1[i].className)
     }
     
+    if(bomb1.length < 5){
+        for (let i = 0; i < 5-bomb1.length; i++) {
+            $("#eloresz .modul").eq(Math.floor(Math.random()*bomb1.length +1)).after($("#templates .ures").clone())
+        }
+        for (let i = 0; i < 6; i++) {
+            $("#templates .ures").clone().appendTo("#hatresz")
+        }
+    }
+    if(bomb1.length === 5){
+        for (let i = 0; i < 6; i++) {
+            $("#templates .ures").clone().appendTo("#hatresz")
+        }
+    }
+    else{
+        for (let i = 0; i < 11-bomb1.length; i++) {
+            $("#templates .ures").clone().appendTo("#hatresz")
+        }
+    }
     function keveres(tomb) {
         var currentIndex = tomb.length, randomIndex;
     
