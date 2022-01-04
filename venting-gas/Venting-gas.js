7b676d8573c17f292c7cc14c3dffbb59d5eec85c
SchmiedmeiszterAdam <smikuadam@gmail.com>
2022-01-04 01:21:34 +0100

    venting gas javítások

--- venting-gas/Venting-gas.js
+++ venting-gas/Venting-gas.js
@@ -6,26 +6,34 @@ class VentingGas extends Modul {
         this.gombok = this.elem.find(".venting-gas-gombok").children()
         this.idoKijelzo = this.elem.find(".venting-gas-ido")
         this.ido
+        this.villogasSzamlalo = 0
+        this.timeout
+        this.aktiv = false
         for (let i = 0; i < this.gombok.length; i++) {
             $(this.gombok[i]).on("click", () => {
-                this.ellenorzes(i)
+                if (this.aktiv) {
+                    this.ellenorzes(i)
+                }
             })
         }
+        this.kerdesAd()
         setTimeout(() => {
             this.kerdesAdas()
         }, 5000)
     }
+    kerdesAd() {
+        let random = Math.floor(Math.random() * questions.length)
+        this.kerdes = questions[random].question
+        this.valasz = questions[random].answer
+    }
     kerdesAdas() {
+        this.aktiv = true
         this.ido = 40
         this.idoInditas()
         this.interval = setInterval(() => {
             this.idoInditas()
         }, 1000);
-        let random = Math.floor(Math.random() * questions.length)
-        let kerdes = questions[random].question
-        this.valasz = questions[random].answer
-        $(this.kijelzo[0]).html(kerdes)
-        $(this.kijelzo[1]).html("Y/N")
+        this.kijelzoreIras(this.kerdes,"Y/N")
     }
     idoInditas() {
         let ido = this.ido
@@ -34,11 +42,11 @@ class VentingGas extends Modul {
         }
         $(this.idoKijelzo).html(ido)
         this.ido--
-        if (this.ido < 0) {
+        if (this.ido < -1) {
+            this.aktiv = false
             clearInterval(this.interval)
             this.sendFault()
-            $(this.kijelzo[0]).html("")
-            $(this.kijelzo[1]).html("")
+            this.kijelzoreIras("","","")
             $(this.idoKijelzo).html("")
             setTimeout(() => {
                 this.kerdesAdas()
@@ -46,34 +54,80 @@ class VentingGas extends Modul {
         }
     }
     ellenorzes(i) {
-        if ((this.valasz === "YES" && i === 0) || (this.valasz === "NO" && i === 1)) {
-            this.vege()
-        } else {
+        if (this.valasz === "NO" && i === 1) {
+            this.valaszKiIr(questions[i].answer)
+            setTimeout(() => {
+                this.vege()
+            }, 1600)
+        }
+        else if (this.valasz === "YES" && i === 0) {
+            this.valaszKiIr(questions[i].answer)
+            setTimeout(() => {
+                this.complete()
+            }, 1600)
+        }
+        else if (this.valasz === "YES" && i === 1) {
+            this.villogasSzamlalo = 0
+            clearTimeout(this.timeout)
+            clearTimeout(this.timeout2)
+            this.valaszKiIr(questions[i].answer)
+            setTimeout(() => {
+                this.vpe()
+            }, 1600)
+        }
+        else {
+            this.aktiv = false
             this.sendFault()
+            this.vege()
         }
     }
-    vege() {
-        clearInterval(this.interval)
-        $(this.idoKijelzo).html("")
-        let valasz = this.valasz
+    valaszKiIr(valasz) {
         let szamlalo = 0
         let interval = setInterval(() => {
             $(this.kijelzo[2]).append(valasz[szamlalo])
             szamlalo++
             if (szamlalo === valasz.length) {
                 clearInterval(interval)
-                setTimeout(() => {
-                    this.complete()
-                }, 1000);
-                setTimeout(() => {
-                    this.kerdesAdas()
-                }, 30000)
             }
         }, 500);
     }
+    vege() {
+        clearInterval(this.interval)
+        $(this.idoKijelzo).html("")
+        this.kijelzoreIras("","","")
+        this.aktiv = false
+        setTimeout(() => {
+            this.kerdesAdas()
+        }, 30000)
+    }
     complete() {
-        $(this.kijelzo[0]).html("VENTING")
-        $(this.kijelzo[1]).html("COMPLETE")
-        $(this.kijelzo[2]).html("")
+        clearInterval(this.interval)
+        $(this.idoKijelzo).html("")
+        this.kijelzoreIras("VENTING","COMPLETE","")
+        setTimeout(() => {
+            this.kerdesAdas()
+        }, 30000)
+    }
+    vpe() {
+        this.aktiv = false
+        if (this.villogasSzamlalo < 3) {
+            this.kijelzoreIras("VENTING","PREVENTS","EXPLOSION")
+            this.timeout = setTimeout(() => {
+                this.kijelzoreIras("","","")
+            }, 1000)
+            this.villogasSzamlalo++
+            this.timeout2 = setTimeout(() => {
+                this.vpe()
+            }, 1500)
+        }
+        else {
+            this.kijelzoreIras(this.kerdes,"Y/N")
+            this.aktiv = true
+        }
+    }
+    kijelzoreIras(sz1, sz2, sz3) {
+        $(this.kijelzo[0]).html(sz1)
+        $(this.kijelzo[1]).html(sz2)
+        $(this.kijelzo[2]).html(sz3)
     }
 }
